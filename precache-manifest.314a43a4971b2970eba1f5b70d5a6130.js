0041ed0ca43aed23ee83c285fdf0ba58594f0462
Artem2313 <huselnykov@gmail.com>
2019-12-25 12:17:44 +0200

    Updates

---
+++ precache-manifest.314a43a4971b2970eba1f5b70d5a6130.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f56ec5df794d2a74bd614ed4e453f33",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "f435968699ca3c5447d6",
+    "url": "/goit-react-hw-01-components/static/js/2.8ed68965.chunk.js"
+  },
+  {
+    "revision": "ef6f2129bd42fdc6ef51",
+    "url": "/goit-react-hw-01-components/static/js/main.22de1e63.chunk.js"
+  },
+  {
+    "revision": "c4036953cb714bd39313",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.02e29828.js"
+  }
+]);
