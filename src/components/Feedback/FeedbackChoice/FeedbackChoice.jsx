02514b184907de2ba89fb0ef5674bf8c2616f97b
Artem2313 <huselnykov@gmail.com>
2020-01-20 11:46:27 +0200

    version_2

--- src/components/Feedback/FeedbackChoice/FeedbackChoice.jsx
+++ src/components/Feedback/FeedbackChoice/FeedbackChoice.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
 
-const FeedbackChoice = ({ options, onLeaveFeedback }) => {
-  return (
-    <div>
-      {Object.keys(options).map(option => (
-        <button
-          type="button"
-          name={option}
-          key={shortid.generate()}
-          onClick={onLeaveFeedback}
-        >
-          {option}
-        </button>
-      ))}
-    </div>
-  );
-};
+const FeedbackChoice = ({ options, onLeaveFeedback }) => (
+  <div>
+    {Object.keys(options).map(option => (
+      <button
+        type="button"
+        name={option}
+        key={shortid.generate()}
+        onClick={onLeaveFeedback}
+      >
+        {option}
+      </button>
+    ))}
+  </div>
+);
 
 FeedbackChoice.propTypes = {
   options: PropTypes.shape({}).isRequired,
